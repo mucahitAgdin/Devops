328cf7fad5aaf7a737dc5c9661ba82f1dc93b6ca
mucahitAgdin <agdinn@gmail.com>
2023-12-18 20:19:44 +0300

    .

--- docker_nodejs/index.js
+++ docker_nodejs/index.js
@@ -1,17 +1,22 @@
-//Load express module with `require` directive
-const express = require('express')
-const app = express()
-
+const express = require('express');
+const app = express();
+const https = require('https');
 require('dotenv').config()
+const fs = require('fs');
+const port = process.env.PORT;
+const cert_key = process.env.CERT_KEY;
+const cert_pem = process.env.CERT_PEM;
 
-//Read port number from .env
-const port = process.env.PORT
-//Define request response in root URL (/)
-app.get('/', function (req, res) {
-    res.send("hello world")
-})
+app.get('/', (req, res) => {
+    res.send("IT'S WORKING!");
+});
 
-//Launch listening server on port 8081
-app.listen(port, function () {
-    console.log('app listening on port ' + port)
-})
+const httpsOptions = {
+    key: fs.readFileSync(cert_key),
+    cert: fs.readFileSync(cert_pem)
+};
+
+const server = https.createServer(httpsOptions, app)
+    .listen(port, () => {
+        console.log('Server running at port ' + port);
+    });
